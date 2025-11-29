0de580687cb6b85f711b4b6890d6ab58853ff2c8
Adam Wilson <2005adamw@gmail.com>
2025-11-29 17:14:16 +0000

    added helper functions for the airport data

--- flightdata.py
+++ flightdata.py
@@ -8,4 +8,21 @@ G_df = pd.read_csv('../input/openflights-route-database-2014/routes.csv',usecols
 cols_list=["City","Country","IATA","Latitude","Longitude"]
 airport_df = pd.read_csv('../input/openflights-airports-database-2017/airports.csv',usecols=cols_list)
 
+airports = airport_df['IATA'].unique().tolist()
 
+
+def get_outbound_from(airport_iata):
+    return G_df[G_df['Source airport'] == airport_iata]
+
+
+def get_flights_in(airport_iata):
+    return G_df[G_df['Destination airport'] == airport_iata]
+
+
+def get_airports():
+    return airports
+
+def get_airport_info(airport_iata):
+    info = airport_df[airport_df['IATA'] == airport_iata]
+
+    return info.to_dict('records')[0]
