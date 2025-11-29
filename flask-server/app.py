1f871ae44fe97f81c5199467821f5fff076a1c96
Harry Vicaradge <harryvic_@outlook.com>
2025-11-29 14:40:16 +0000

    inital commit

---
+++ flask-server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Members API Route example
+@app.route("/members")
+def members():
+    return{"members": ["Member1"]}
+
+if __name__ =="__main__":
+    app.run(debug=True)
