fe3dfa95984fd27399b2e0b0e886af6d7cdf6894
Harry Vicaradge <harryvic_@outlook.com>
2025-11-29 20:25:56 +0000

    fixed double import

--- banana/src/components/Earth.tsx
+++ banana/src/components/Earth.tsx
@@ -17,7 +17,7 @@ function Earth() {
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', border: '2px solid red' }}>
       <Globe
         ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-water.png"
         backgroundColor="#000000"
       />
     </div>
