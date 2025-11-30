6de1616c1bf92bb272ba86f50b4c21582e80422e
Adam Wilson <2005adamw@gmail.com>
2025-11-30 00:38:46 +0000

    Work to the player representation

--- flask-server/player.py
+++ flask-server/player.py
@@ -1,10 +1,12 @@
 class Player:
-    def __init__(self, name, location):
+    def __init__(self, userid, name, location):
+        self.id = userid
         self.name = name
         self.location = location
     
     def to_dict(self):
         return {
-            'name': name,
-            'location': location
+            'id': self.id,
+            'name': self.name,
+            'location': self.location
         }
